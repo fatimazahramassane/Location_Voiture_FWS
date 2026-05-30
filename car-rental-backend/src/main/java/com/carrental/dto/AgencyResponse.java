6f300ba4997bbb52c1acf43fbd33c6654aa39745
fatimazahramassane <fatimazahra.massane-etu@etu.univh2c.ma>
2026-05-30 22:54:39 +0100

    feat: DTOs AgencyRequest et AgencyResponse avec validations

---
+++ car-rental-backend/src/main/java/com/carrental/dto/AgencyResponse.java
@@ -0,0 +1,19 @@
+package com.carrental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgencyResponse {
+
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String phone;
+    private String email;
+}
