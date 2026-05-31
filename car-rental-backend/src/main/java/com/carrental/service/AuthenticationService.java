5eec6b79d0cad85a38a291817e9fffe49d697fce
fatimazahramassane <fatimazahra.massane-etu@etu.univh2c.ma>
2026-05-31 02:35:00 +0100

    api: AuthenticationController + JWT login/register/profil

---
+++ car-rental-backend/src/main/java/com/carrental/service/AuthenticationService.java
@@ -0,0 +1,16 @@
+package com.carrental.service;
+
+import com.carrental.dto.AuthResponse;
+import com.carrental.dto.LoginRequest;
+import com.carrental.dto.RegisterRequest;
+import com.carrental.entity.AppUser;
+import org.springframework.security.core.Authentication;
+
+public interface AuthenticationService {
+
+    AppUser register(RegisterRequest request);
+
+    AuthResponse login(LoginRequest request);
+
+    AppUser getProfile(Authentication authentication);
+}
